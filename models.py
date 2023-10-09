82e7cc8f2faa71bd733bd83542899c4a7d7787ec
ariasl <ariasl@utb.edu.co>
2023-10-08 20:46:37 -0500

    db atributes content

---
+++ models.py
@@ -0,0 +1,12 @@
+# models.py
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
